f53190fc344229802badf5a45f278b29daced0db
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-11-06 16:52:26 -0500

    final final commit...getting closer

--- currency.py
+++ currency.py
@@ -5,7 +5,8 @@ def convert(rates, value, current, to):
     for conversion in rates:
         if current == conversion[0] and to == conversion[1]:
             return value * conversion[2]
-    if current == conversion[1] and to == conversion[0]:
-        return value / conversion[2]
+
+        if current == conversion[1] and to == conversion[0]:
+            return value / conversion[2]
 
         raise ValueError(f"can't convert from {current} to {to}")
--- test_currency.py
+++ test_currency.py
@@ -8,27 +8,27 @@ def test_convert_same_currency():
 
 
 def test_convert_using_multiplication():
-    assert convert(
+    assert(convert(
         rates=[("USD", "EUR", 0.74)], value=1, current='USD',
-        to='EUR') == 0.74
-    assert convert(
-        rates=[("EUR", "JPY", 129.22)], value=3, current='EUR',
-        to='JPY') == 387.75
+        to='EUR')) == 0.74
+    assert(convert(
+        rates=[("EUR", "JPY", 129.24)], value=3, current='EUR',
+        to='JPY')) == 3 * 129.24
 
 
 def test_convert_using_division():
-    assert convert(
+    assert(convert(
         rates=[("USD", "EUR", 0.74)], value=1, current='EUR',
-        to='USD') == 1.35
-    assert convert(
+        to='USD')) == 1 / 0.74
+    assert(convert(
         rates=[("EUR", "JPY", 129.24)], value=2, current='JPY',
-        to='EUR') == 0.015
+        to='EUR')) == 2 / 129.24
 
 
 def test_convert_multiple_rates():
     rates = [("USD", "EUR", 0.74), ("EUR", "JPY", 129.24)]
-    assert round(convert(rates, value=5, current='USD', to='EUR'), 2) == 4.38
-    assert round(convert(rates, value=7, current='EUR', to='USD'), 2) == 7.99
-    assert round(convert(rates, value=9, current='EUR', to='JPY'),
-                 2) == 1163.35
+    assert round(convert(rates, value=5, current='USD', to='EUR'), 2) == 3.7
+    assert round(convert(rates, value=7, current='EUR', to='USD'), 2) == 9.46
+    assert round(
+        convert(rates, value=9, current='EUR', to='JPY'), 2) == 1163.35
     assert round(convert(rates, value=11, current='JPY', to='EUR'), 2) == 0.085
